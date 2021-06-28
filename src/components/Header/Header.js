7f140f572c269e4b7b332ff6a3d94d07554b6a39
iulianstan <iulian1226@gmail.com>
2021-06-28 11:39:44 +0200

    Adding logo in Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import Logo from "../../logo.json";
 
 const Header = () => {
+  const logo = Logo.map((obj) => {
+    const { logo } = obj;
+    return <img className="logo" alt="logo" src={logo}></img>;
+  });
   return (
     <React.Fragment>
+      {logo}
       <Navbar />
     </React.Fragment>
   );
