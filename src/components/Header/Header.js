e2b9c064fb4662e6741f37bf66c7d5922292ffaf
iulianstan <iulian1226@gmail.com>
2021-06-28 11:41:46 +0200

    Adding key and implement id from logo.json in Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import Logo from "../../logo.json";
 
 const Header = () => {
   const logo = Logo.map((obj) => {
-    const { logo } = obj;
-    return <img className="logo" alt="logo" src={logo}></img>;
+    const { id, logo } = obj;
+    return <img key={id} className="logo" alt="logo" src={logo}></img>;
   });
   return (
     <React.Fragment>
