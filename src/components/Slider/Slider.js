b82f77b27d16f7b6a5be35e65564bc617ef5fc7e
Kenan Alnaser <kenan-na@live.com>
2021-06-28 14:58:49 +0200

    converting slider to better format

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,31 +1,41 @@
 import React from 'react';
+import Slider from '../../slider.json';
 
 const Slider = () => {
+  const slider = Slider.map((obj) => {
+    const { id, img, content } = obj;
+    return (
+      <div className="showSlide fade" id={id}>
+        <img src={img} />
+        <div className="content">{content}</div>
+      </div>
+    );
+  });
   let slide_index = 1;
-  //   displaySlides(slide_index);
+  displaySlides(slide_index);
+
   function nextSlide(n) {
     displaySlides((slide_index += n));
+
+    function displaySlides(n) {
+      let i;
+      let slides = document.getElementsByClassName('showSlide');
+      if (n > slides.length) {
+        slide_index = 1;
+      }
+      if (n < 1) {
+        slide_index = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+      }
+      slides[slide_index - 1].style.display = 'block';
+    }
   }
   return (
     <React.Fragment>
       <div className="slidercontainer">
-        <div className="showSlide fade">
-          <img src="images/img1.jpg" />
-          <div className="content">Lorem ipsum dolor sit amet</div>
-        </div>
-        <div className="showSlide fade">
-          <img src="images/img2.jpg" />
-          <div className="content">Lorem ipsum dolor sit amet</div>
-        </div>
-
-        <div className="showSlide fade">
-          <img src="images/img3.jpg" />
-          <div className="content">Lorem ipsum dolor sit amet</div>
-        </div>
-        <div className="showSlide fade">
-          <img src="images/img4.jpg" />
-          <div className="content">Lorem ipsum dolor sit amet</div>
-        </div>
+        {slider}
         {/* <!-- Navigation arrows -->   */}
         <a className="left" onclick={nextSlide(-1)}>
           ❮
