719921c3fe0ec8b99f89e90e98cd70e1ace82186
Kenan Alnaser <kenan-na@live.com>
2021-06-28 11:16:07 +0200

    creating html slider baseline

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Slider = () => {
+  return (
+    <React.Fragment>
+      <div class="slidercontainer">
+        <div class="showSlide fade">
+          <img src="images/img1.jpg" />
+          <div class="content">Lorem ipsum dolor sit amet</div>
+        </div>
+        <div class="showSlide fade">
+          <img src="images/img2.jpg" />
+          <div class="content">Lorem ipsum dolor sit amet</div>
+        </div>
+
+        <div class="showSlide fade">
+          <img src="images/img3.jpg" />
+          <div class="content">Lorem ipsum dolor sit amet</div>
+        </div>
+        <div class="showSlide fade">
+          <img src="images/img4.jpg" />
+          <div class="content">Lorem ipsum dolor sit amet</div>
+        </div>
+        {/* <!-- Navigation arrows -->   */}
+        <a class="left" onclick="nextSlide(-1)">
+          ❮
+        </a>
+        <a class="right" onclick="nextSlide(1)">
+          ❯
+        </a>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Slider;
