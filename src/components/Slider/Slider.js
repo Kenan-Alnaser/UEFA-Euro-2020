2efd808e9e13fa8013344000c890660fbf136946
Kenan Alnaser <kenan-na@live.com>
2021-06-28 13:00:56 +0200

    updating slight syntax error

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -8,29 +8,29 @@ const Slider = () => {
   }
   return (
     <React.Fragment>
-      <div class="slidercontainer">
-        <div class="showSlide fade">
+      <div className="slidercontainer">
+        <div className="showSlide fade">
           <img src="images/img1.jpg" />
-          <div class="content">Lorem ipsum dolor sit amet</div>
+          <div className="content">Lorem ipsum dolor sit amet</div>
         </div>
-        <div class="showSlide fade">
+        <div className="showSlide fade">
           <img src="images/img2.jpg" />
-          <div class="content">Lorem ipsum dolor sit amet</div>
+          <div className="content">Lorem ipsum dolor sit amet</div>
         </div>
 
-        <div class="showSlide fade">
+        <div className="showSlide fade">
           <img src="images/img3.jpg" />
-          <div class="content">Lorem ipsum dolor sit amet</div>
+          <div className="content">Lorem ipsum dolor sit amet</div>
         </div>
-        <div class="showSlide fade">
+        <div className="showSlide fade">
           <img src="images/img4.jpg" />
-          <div class="content">Lorem ipsum dolor sit amet</div>
+          <div className="content">Lorem ipsum dolor sit amet</div>
         </div>
         {/* <!-- Navigation arrows -->   */}
-        <a class="left" onclick={nextSlide(-1)}>
+        <a className="left" onclick={nextSlide(-1)}>
           ❮
         </a>
-        <a class="right" onclick={nextSlide(1)}>
+        <a className="right" onclick={nextSlide(1)}>
           ❯
         </a>
       </div>
