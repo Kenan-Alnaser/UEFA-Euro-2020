2290acfeb84c92ec49ce5ecda78a1dd65c83b797
Kenan Alnaser <kenan-na@live.com>
2021-06-29 10:53:08 +0200

    changing slider style

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -5,13 +5,21 @@ import { BsArrowLeft } from 'react-icons/bs';
 
 const Slider = () => {
   const [value, setValue] = useState(0);
+  const sliderData = Data.map((obj) => {
+    const { id, img, content } = obj;
+    return (
+      <div>
+        <img src={img} key={id} />
+        <h2>{content}</h2>
+      </div>
+    );
+  });
 
   return (
     <React.Fragment>
-      <div className="container">
-        <img src={Data.img} alt={Data.id} />
-        <h2>{Data.content}</h2>
-        <div>
+      <div className="mainContainer">
+        <div className="container">
+          {sliderData}
           <button type="button" onClick={() => (value === 0 ? setValue(3) : setValue(value - 1))}>
             <BsArrowLeft />
           </button>
