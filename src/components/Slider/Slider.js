417eb81eb4306e34f801bef0176d6dfa7e1bf2ed
Kenan Alnaser <kenan-na@live.com>
2021-06-29 10:34:40 +0200

    changing the whole slider to a simple cleaner one

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,52 +1,24 @@
-import React from 'react';
-import Slider from '../../slider.json';
+import React, { useState } from 'react';
+import Data from '../../slider.json';
+import { BsArrowRightShort } from 'react-icons/bs';
+import { BsArrowLeft } from 'react-icons/bs';
 
 const Slider = () => {
-  const slider = Slider.map((obj) => {
-    const { id, img, content } = obj;
-    return (
-      <div className="showSlide fade" id={id}>
-        <img src={img} />
-        <div className="content">{content}</div>
-      </div>
-    );
-  });
-  let slide_index = 1;
-  displaySlides(slide_index);
+  const [value, setValue] = useState(0);
 
-  function nextSlide(n) {
-    displaySlides((slide_index += n));
-
-    // function currentSlide(n) {
-    //   displaySlides((slide_index = n));
-    // }
-
-    function displaySlides(n) {
-      let i;
-      let slides = document.getElementsByClassName('showSlide');
-      if (n > slides.length) {
-        slide_index = 1;
-      }
-      if (n < 1) {
-        slide_index = slides.length;
-      }
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-      }
-      slides[slide_index - 1].style.display = 'block';
-    }
-  }
   return (
     <React.Fragment>
-      <div className="slidercontainer">
-        {slider}
-        {/* <!-- Navigation arrows -->   */}
-        <a className="left" onclick={nextSlide(-1)}>
-          ❮
-        </a>
-        <a className="right" onclick={nextSlide(1)}>
-          ❯
-        </a>
+      <div className="container">
+        <img src={Data.img} alt={Data.id} />
+        <h2>{Data.content}</h2>
+        <div>
+          <button type="button" onClick={() => (value === 0 ? setValue(3) : setValue(value - 1))}>
+            <BsArrowLeft />
+          </button>
+          <button type="button" onClick={() => (value === 3 ? setValue(0) : setValue(value + 1))}>
+            <BsArrowRightShort />
+          </button>
+        </div>
       </div>
     </React.Fragment>
   );
