3070bc6d39e5e760560072b7039cae8b3b2edff7
Kenan Alnaser <kenan-na@live.com>
2021-06-29 11:18:55 +0200

    changing slider once more

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,25 +1,17 @@
 import React, { useState } from 'react';
-import Data from '../../slider.json';
+import { arr } from './sliderData.js';
 import { BsArrowRightShort } from 'react-icons/bs';
 import { BsArrowLeft } from 'react-icons/bs';
 
 const Slider = () => {
   const [value, setValue] = useState(0);
-  const sliderData = Data.map((obj) => {
-    const { id, img, content } = obj;
-    return (
-      <div>
-        <img src={img} key={id} />
-        <h2>{content}</h2>
-      </div>
-    );
-  });
 
   return (
     <React.Fragment>
-      <div className="mainContainer">
-        <div className="container">
-          {sliderData}
+      <div className="mainSliderContainer">
+        <div className="sliderContainer">
+          <img src={arr[value].img} alt={arr[value].id} />
+          <h2>{arr[value].content}</h2>
           <button type="button" onClick={() => (value === 0 ? setValue(3) : setValue(value - 1))}>
             <BsArrowLeft />
           </button>
---
+++ src/components/Slider/sliderData.js
@@ -0,0 +1,22 @@
+export const arr = [
+  {
+    id: 0,
+    img: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/1F36/production/_118109970_livreal.jpg',
+    content: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 1,
+    img: 'https://www.worcester.gov.uk/images/easyblog_shared/2019/b2ap3_large_Football---carousel.jpg',
+    content: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 2,
+    img: 'https://i.ytimg.com/vi/GvxlcBJCrTs/maxresdefault.jpg',
+    content: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 3,
+    img: 'https://o7dkx1gd2bwwexip1qwjpplu-wpengine.netdna-ssl.com/wp-content/uploads/2019/06/Screen-Shot-2019-06-19-at-12.50.57-PM.png',
+    content: 'lorem ipsum dolor sit amet',
+  },
+];
