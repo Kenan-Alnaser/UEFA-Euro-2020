05d67fb3abfb8c3f9b2d874aad1dd67bce7d0646
iulianstan <iulian1226@gmail.com>
2021-06-30 12:17:08 +0200

    Adding slider to Groups component

--- src/components/Groups/Groups.js
+++ src/components/Groups/Groups.js
@@ -1,9 +1,17 @@
 // Import area
-import React from 'react';
+import React, { Component } from 'react';
+import Slider from 'react-slick';
 import TeamGroup from '../Group/TeamGroup';
 
 // Groups function
 const Groups = ({ groups }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
   return (
     <React.Fragment>
       <div>
@@ -12,31 +20,33 @@ const Groups = ({ groups }) => {
           <button>{'<'}</button>
           <button>{'>'}</button>
         </div>
-        <div>
-          <h2>Group A</h2>
-          <TeamGroup group="Group A" />
-        </div>
-        <div>
-          <h2>Group B</h2>
-          <TeamGroup group="Group B" />
-        </div>
-        <div>
-          <h2>Group C</h2>
-          <TeamGroup group="Group C" />
-        </div>
-        <div>
-          <h2>Group D</h2>
-          <TeamGroup group="Group D" />
-        </div>
-        <div>
-          <h2>Group E</h2>
-          <TeamGroup group="Group E" />
-        </div>
+        <Slider {...settings}>
+          <div>
+            <h2>Group A</h2>
+            <TeamGroup group="Group A" />
+          </div>
+          <div>
+            <h2>Group B</h2>
+            <TeamGroup group="Group B" />
+          </div>
+          <div>
+            <h2>Group C</h2>
+            <TeamGroup group="Group C" />
+          </div>
+          <div>
+            <h2>Group D</h2>
+            <TeamGroup group="Group D" />
+          </div>
+          <div>
+            <h2>Group E</h2>
+            <TeamGroup group="Group E" />
+          </div>
 
-        <div>
-          <h2>Group F</h2>
-          <TeamGroup group="Group F" />
-        </div>
+          <div>
+            <h2>Group F</h2>
+            <TeamGroup group="Group F" />
+          </div>
+        </Slider>
       </div>
     </React.Fragment>
   );
